2976faa43919748c2d6f95499bff1c58290e4adb
SumanJS <sm7135120@gmail.com>
2024-08-21 00:45:15 +0530

    feat: added middleware and auth config

--- auth.ts
+++ auth.ts
@@ -1,8 +1,13 @@
-// import NextAuth from "next-auth";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
-// import { prisma } from "@/lib/prisma";
+import authConfig from "@/auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import { db } from "./lib/db";
 
-// export const { handlers, auth, signIn, signOut } = NextAuth({
-//   adapter: PrismaAdapter(prisma),
-//   providers: [],
-// });
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
